785e56c968e922d78f403b6d33a2f3d7c9c6af38
corbatsh <bortlikkamil@gmail.com>
2022-06-02 22:52:33 +0200

    Snapshots

---
+++ cypress/integration/image.spec.js
@@ -0,0 +1,28 @@
+const pages = ["http://example.com/"]
+
+const sizes = ["iphone-6", "ipad-2", [1200,800]]
+
+describe('Visual Regression', () => {
+	sizes.forEach(size => {
+		pages.forEach(page => {
+			it(`Should match ${page} in resolution ${size}`, () => {
+				let currentTime = new Date(Date.UTC(2022, 1,1)).getDate()
+				cy.clock(currentTime)
+				cy.setResolution(size)
+				cy.visit(page)
+				cy.matchImageSnapshot()
+			})
+		})
+	})
+})
+
+describe('Single Element Snapshot', () => {
+	it('Should match a single element on the page', () => {
+		cy.visit('http://example.com/')
+		cy.get('h1').matchImageSnapshot({
+			failureTreshold: 10,
+			failureTresholdType: "pixels"
+		})
+
+	})
+})
